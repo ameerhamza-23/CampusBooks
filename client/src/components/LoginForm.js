1c799686658c372455586ebdde29c6c493b11c6a
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-11-11 00:35:33 +0500

    Initialized router

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -7,7 +7,7 @@ function LoginForm() {
                 <img src='/images/log.png' alt='login' width={200}></img>
                 <input style={{backgroundColor:'#303134'}} type='email' placeholder='   Email' className='border-2 border-black rounded-md p-3 w-96'/>
                 <input style={{backgroundColor:'#303134'}} type='password' placeholder='   Password' className='border-2 border-black rounded-md p-3 w-96'/>
-                <button className='p-4 rounded-lg text-white font-bold w-96 bg-purple-600 hover:bg-purple-500' style={{}}>Login</button>
+                <button className='p-4 rounded-lg text-white font-bold w-96 bg-purple-600 hover:bg-purple-500 transition-all' style={{}}>Login</button>
             </div>
     </div>
   )
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     return (
             <div className='flex justify-end items-end h-20  font-semibold'>
                 <ul className='grid grid-cols-3 gap-5 text-white mr-10'>
-                    <span className='flex items-end justify-center hover:cursor-pointer' ><WiMoonWaningCrescent5 size={30}/></span>
+                    <span className='flex items-end justify-center hover:cursor-pointer transform hover:rotate-180 transition duration-300' ><WiMoonWaningCrescent5 size={30}/></span>
                     <Link to="/login" className='border-2 border-purple-500 rounded-md px-3 py-1'>Login</Link>
                     <Link to="/signup" className='bg-purple-500 px-3 py-1 rounded-md'>Sign Up</Link>
                 </ul>
