8472e05e14ef5fd69668517d903469443cb71c9b
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-11-10 23:55:56 +0500

    Added moon icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {WiDaySunny} from 'react-icons/wi'
-
+import {WiDaySunny, WiMoonWaningCrescent5 } from 'react-icons/wi'
 
 function Navbar() {
+    
+
     return (
             <div className='flex justify-end items-end h-20  font-semibold'>
-                <ul className='grid grid-cols-3 text-white mr-20'>
-                    <span className='flex items-end justify-center hover:cursor-pointer'><WiDaySunny /></span>
-                    <Link to="/login">Login &nbsp; &nbsp; | &nbsp;</Link>
-                    <button>Sign Up</button>
+                <ul className='grid grid-cols-3 gap-5 text-white mr-10'>
+                    <span className='flex items-end justify-center hover:cursor-pointer' ><WiMoonWaningCrescent5 size={30}/></span>
+                    <Link to="/login" className='border-2 border-purple-500 rounded-md px-3 py-1'>Login</Link>
+                    <Link to="/signup" className='bg-purple-500 px-3 py-1 rounded-md'>Sign Up</Link>
                 </ul>
             </div>
     )
