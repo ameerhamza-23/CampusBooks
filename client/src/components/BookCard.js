160aed6efbeb7f2a43edf594fcf594bfb8bf4663
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-23 01:21:33 +0500

    add wishlist icon

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,6 +1,15 @@
+import React, { useState } from "react";
 import Avatar from "./Avatar";
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 
 export default function BookCard() {
+
+  const [flag, setFlag] = useState(false)
+  const addToWishlist = () => {
+    setFlag(!flag)
+  }
+
   return (
     <div className="w-76 relative">
       <div className="w-76 h-64 overflow-hidden">
@@ -11,7 +20,7 @@ export default function BookCard() {
       <div>
         <div className="flex justify-between p-4 text-lg rounded-bl-lg rounded-br-lg" style={{backgroundColor:'#303134'}}>
             <h1 className="font-bold">Rs 500</h1>
-             <h1>logo</h1>
+             <span onClick={addToWishlist} className="flex items-center hover:cursor-pointer">{!flag ? <FaRegHeart/> : <FaHeart color={'#9370DB'}/>}</span>
         </div>
       </div>
       <div className="absolute -top-5 -right-5">
