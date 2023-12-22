994b19f65810b3d92fd1b2727c67d795b4d547fe
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-22 22:09:24 +0500

    complete avatar

---
+++ client/src/components/Avatar.js
@@ -0,0 +1,9 @@
+export default function Avatar() {
+  return (
+
+    <div className="w-14 h-14 rounded-full bg-green-900">
+
+    </div>
+
+  )
+}
--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,16 +1,22 @@
+import Avatar from "./Avatar";
+
 export default function BookCard() {
   return (
-    <div className="w-80 h-64 bg-red-400">
-      <div className="img-div h-48">
-
+    <div className="w-80 h-64 relative">
+      <div className="img-div w-80 h-48 overflow-hidden">
+        <img src="images/book2.jpeg" alt="book image" className="w-full h-full object-fill" />
       </div>
-      <div className="card-footer h-16 flex justify-between text-center">
+
+      <div className="card-footer h-16 rounded-br-lg rounded-bl-lg flex justify-between text-center" style={{ backgroundColor: '#303134' }}>
         <div className="h-full flex items-center px-4">
-          PKR 500
+          PKR. 500
         </div>
         <div className="h-full flex items-center px-4">
-          Logo
+          logo
         </div>
+      </div>
+      <div className="avatar absolute -top-5 -right-5">
+        <Avatar />
       </div>
     </div >
   )
