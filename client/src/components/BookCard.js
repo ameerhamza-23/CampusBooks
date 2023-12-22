6d1ed4483b3f90c325e3d929d4b692aadf3f073c
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-22 22:28:55 +0500

    search bar

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -4,12 +4,12 @@ export default function BookCard() {
   return (
     <div className="w-80 h-64 relative">
       <div className="img-div w-80 h-48 overflow-hidden">
-        <img src="images/book2.jpeg" alt="book image" className="w-full h-full object-fill" />
+        <img src="images/book2.jpeg" alt="book image" className="w-full h-full object-cover" />
       </div>
 
       <div className="card-footer h-16 rounded-br-lg rounded-bl-lg flex justify-between text-center" style={{ backgroundColor: '#303134' }}>
         <div className="h-full flex items-center px-4">
-          PKR. 500
+          Rs 500
         </div>
         <div className="h-full flex items-center px-4">
           logo
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+export default function SearchBar() {
+  return (
+
+    <div className="rounded-full w-96 h-10 flex items-center" style={{ backgroundColor: '#303134' }}>
+      <input placeholder="Search" className="ml-4 w-3/4 focus:outline-none" style={{ backgroundColor: '#303134' }} />
+    </div>
+
+  )
+}
