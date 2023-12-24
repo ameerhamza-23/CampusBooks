3bb28ec23b8f7717b522961898d40ac886891ab6
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-24 16:52:56 +0500

    setup user table

--- client/src/components/admin/Users.js
+++ client/src/components/admin/Users.js
@@ -1,24 +1,41 @@
+import Avatar from "../Avatar"
 export default function Users() {
   return (
 
     <div>
 
-      <div className='p-10'>
+      <div className='p-10 flex justify-center items-center'>
 
-        <div className='table pt-10'>
-          <table className="min-w-full border border-gray-300">
+        <div className='sm:w-1/2 md:w-3/4 lg:w-full'>
+          <table>
             <thead>
               <tr>
-                <th className="py-2 px-4 border-b">ID</th>
-                <th className="py-2 px-4 border-b">Name</th>
-                <th className="py-2 px-4 border-b">Email</th>
+                <th className="py-4 px-4 ">ID</th>
+                <th className="py-4 px-4 ">Image</th>
+                <th className="py-4 px-4 ">Name</th>
+                <th className="py-4 px-4 ">Email</th>
+                <th className="py-4 px-4 ">Phone</th>
+                <th className="py-4 px-4 ">University</th>
+                <th className="py-4 px-4 ">Campus</th>
+                <th className="py-4 px-4 ">Account Status</th>
+                <th className="py-4 px-4 ">Member Since</th>
+                <th className="py-4 px-4 ">Edit</th>
+                <th className="py-4 px-4 ">Delete</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
+              <tr className="text-center">
                 <td className="py-2 px-4 border-b">1</td>
+                <td className="py-2 px-4 border-b"><Avatar/></td>
                 <td className="py-2 px-4 border-b">John Doe</td>
                 <td className="py-2 px-4 border-b">john.doe@example.com</td>
+                <td className="py-2 px-4 border-b">+923014734130</td>
+                <td className="py-2 px-4 border-b">+FAST</td>
+                <td className="py-2 px-4 border-b">Lahore</td>
+                <td className="py-2 px-4 border-b">Active</td>
+                <td className="py-2 px-4 border-b">23-11-2001</td>
+                <td className="py-2 px-4 border-b"><button>edit-btn</button></td>
+                <td className="py-2 px-4 border-b"><button>delete-btn</button></td>
               </tr>
             </tbody>
           </table>
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import  Users  from "../components/admin/Users"
+import  Books  from "../components/admin/Books"
 
 export default function Admin() {
 
@@ -7,13 +9,17 @@ export default function Admin() {
   const [tab, setTab] = useState("users");
 
   return (
-    <div className='p-10'>
+    <div className='p-10' style={{height:'80vh'}}>
       <div className='breadcrumbs flex gap-3'>
         <button className={`${underlineStyle}`}>Home</button>
         <p>/</p>
-        <button className={`${underlineStyle}`}>Users</button>
+        <button className={`${underlineStyle}`} onClick={() => setTab("users")}>Users</button>
         <p>/</p>
-        <button className={`${underlineStyle}`}>Admin</button>
+        <button className={`${underlineStyle}`} onClick={() => setTab("books")}>Admin</button>
+      </div>
+
+      <div className='w-3/4 h-full rounded-lg mx-auto my-10 overflow-x-auto overflow-y-auto' style={{ backgroundColor: '#303134' }}>
+        {tab === "users" ? <Users /> : <Books />}
       </div>
     </div>
   )
