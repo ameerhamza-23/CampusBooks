db20fb30e59c157a2448489adae85594a114f44a
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-24 18:07:48 +0500

    style filter button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BookCard from '../components/BookCard'
 import SearchBar from '../components/SearchBar'
+import { IoFilter } from "react-icons/io5";
 
 function Home() {
   return (
@@ -8,7 +9,7 @@ function Home() {
 
       <div className='mt-4 flex justify-center items-center sm:justify-between'>
         <SearchBar />
-        <h1>filter</h1>
+        <span><button className='border border-purple-500 lg:mr-5 rounded-full px-6 hover:bg-purple-500 py-2 flex items-center gap-2'>filter<IoFilter /></button></span>
       </div>
 
       <div className='mt-14 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
