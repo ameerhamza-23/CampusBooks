960793a5e07a4a765ec4b4f1f233ce3b32b18b17
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-24 17:35:03 +0500

    complete books table

--- client/src/components/admin/Books.js
+++ client/src/components/admin/Books.js
@@ -2,7 +2,7 @@ import Avatar from "../Avatar"
 import { MdModeEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 
-export default function Users() {
+export default function Books() {
   return (
 
 
@@ -16,25 +16,27 @@ export default function Users() {
               <th className="py-4 px-4 ">Image</th>
               <th className="py-4 px-4 ">Name</th>
               <th className="py-4 px-4 ">Author</th>
-              <th className="py-4 px-4 ">Phone</th>
-              <th className="py-4 px-4 ">University</th>
-              <th className="py-4 px-4 ">Campus</th>
-              <th className="py-4 px-4 ">Account Status</th>
-              <th className="py-4 px-4 ">Member Since</th>
-              <th className="py-4 px-4 ">Edit</th>
-              <th className="py-4 px-4 ">Delete</th>
+              <th className="py-4 px-4 ">Genre</th>
+              <th className="py-4 px-4 ">Price</th>
+              <th className="py-4 px-4">Conition</th>
+              <th className="py-4 px-4">Avaliability</th>
+              <th className="py-4 px-4">Uploaded By</th>
+              <th className="py-4 px-4">Upload Date</th>
+              <th className="py-4 px-4">Edit Book</th>
+              <th className="py-4 px-4">Delete Book</th>
             </tr>
           </thead>
           <tbody>
             <tr className="text-center">
               <td className="py-2 px-4 border-b">1</td>
               <td className="py-2 px-4 border-b"><Avatar /></td>
-              <td className="py-2 px-4 border-b">John Doe</td>
-              <td className="py-2 px-4 border-b">john.doe@example.com</td>
-              <td className="py-2 px-4 border-b">+923014734130</td>
-              <td className="py-2 px-4 border-b">+FAST</td>
-              <td className="py-2 px-4 border-b">Lahore</td>
-              <td className="py-2 px-4 border-b">Active</td>
+              <td className="py-2 px-4 border-b">Algorithms</td>
+              <td className="py-2 px-4 border-b">Tahir Ejaz</td>
+              <td className="py-2 px-4 border-b">Computing</td>
+              <td className="py-4 px-4 border-b">Rs 500</td>
+              <td className="py-4 px-4 border-b">New</td>
+              <td className="py-2 px-4 border-b">Avaliable</td>
+              <td className="py-2 px-4 border-b">Hamza</td>
               <td className="py-2 px-4 border-b">23-11-2001</td>
               <td className="py-2 px-4 border-b"><button><MdModeEdit size={25} /></button></td>
               <td className="py-2 px-4 border-b"><button><MdDelete size={25} /></button></td>
