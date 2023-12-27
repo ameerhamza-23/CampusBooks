8eed7285d60d6eda913abac87c8ce6ee2584b0c8
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-28 02:48:31 +0500

    responsive layout for mobile sizes

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -11,20 +11,20 @@ export default function BookCard() {
   }
 
   return (
-    <div className="w-56 relative">
+    <div className="w-56 relative mx-auto sm:mx-0">
       <div className="w-56 h-64 overflow-hidden">
 
         <img src="images/book2.jpeg" alt="book image" className="w-full h-full object-cover" />
 
       </div >
       <div>
-        <div className="flex justify-between p-4 text-lg rounded-bl-lg rounded-br-lg" style={{backgroundColor:'#303134'}}>
-            <h1 className="font-bold">Rs 500</h1>
-             <span onClick={addToWishlist} className="flex items-center hover:cursor-pointer">{!flag ? <FaRegHeart/> : <FaHeart color={'#9370DB'}/>}</span>
+        <div className="flex justify-between p-4 text-lg rounded-bl-lg rounded-br-lg" style={{ backgroundColor: '#303134' }}>
+          <h1 className="font-bold">Rs 500</h1>
+          <span onClick={addToWishlist} className="flex items-center hover:cursor-pointer">{!flag ? <FaRegHeart /> : <FaHeart color={'#9370DB'} />}</span>
         </div>
       </div>
       <div className="absolute -top-5 -right-5">
-          <Avatar/>
+        <Avatar />
       </div>
     </div>
 
