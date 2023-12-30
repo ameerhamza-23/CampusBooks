ecb45ddc0f08e2eb0b379aaacdcf3b940780dd97
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-31 02:41:01 +0500

    Added login functionalit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,6 +28,18 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
 
+    const {username, password} = req.body;
+    const query = `SELECT * FROM users WHERE username = $1`;
+    const checkUserExists = await pool.query(query,[username]);
+    if (!checkUserExists.rows.length > 0 ) {
+        return res.status(400).json({ error: 'Username or password is incorrect' });
+    }   
+    const user = checkUserExists.rows[0];
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        return res.status(400).json({ error: 'Username or password is incorrect' });
+    }
+    res.json(user);
 }
 
 module.exports = { register, login };
