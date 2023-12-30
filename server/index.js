b474a74dd90888af474c9e0d58ff2f3ea1552bcd
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-30 16:51:38 +0500

    setup mongo database

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log('server is running');
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log("connected to database and listening for requests");
+    })
+  })
 
