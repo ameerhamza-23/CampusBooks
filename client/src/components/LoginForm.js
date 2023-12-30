bf5c0de58c67ba32a5c629e87b16b4a2e717a30d
ameerhamza-23 <hamzaiscoding@gmail.com>
2023-12-31 03:41:46 +0500

    Hooked login form with backend

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import {axios} from "axios"
+import axios from "axios"
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function LoginForm() {
 
-  const handleLogin = () => { 
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleLogin = async() => { 
     
+    try {
+      const {username, password} = user;
+      const newUser = await axios.post('http://localhost:4000/api/auth/login', {username, password});
+      console.log(newUser.data);
+      navigate('/home');
+    }
+    catch(err) {
+      console.log(err.message);
+    }
 
 
   }
@@ -15,8 +33,8 @@ function LoginForm() {
     <div className='form flex h-full items-center'> 
             <div className='flex flex-col gap-4 items-center text-white pb-20'> 
                 <img src='/images/log.png' alt='login' width={200}></img>
-                <input style={isDarkMode ? {backgroundColor:'#303134', color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type='email' placeholder='   Email' className='border-2 border-black rounded-md p-3 w-96'/>
-                <input style={isDarkMode ? {backgroundColor:'#303134',color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type='password' placeholder='   Password' className='border-2 border-black rounded-md p-3 w-96'/>
+                <input style={isDarkMode ? {backgroundColor:'#303134', color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type='text' placeholder='   Username' className='border-2 border-black rounded-md p-3 w-96' onChange={(e) => setUser({...user, username:e.target.value})} value={user.username}/>
+                <input style={isDarkMode ? {backgroundColor:'#303134',color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type='password' placeholder='   Password' className='border-2 border-black rounded-md p-3 w-96' onChange={(e) => setUser({...user, password:e.target.value})} value={user.password}/>
                 <button className='p-4 rounded-lg text-white font-bold w-96 bg-purple-600 hover:bg-purple-500 transition-all' style={{}} onClick={handleLogin}>Login</button>
             </div>
     </div>
