86c61bafa4f45912db93449ce7d699bf42033462
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-01-15 14:58:34 +0500

    Book input form complete

--- client/src/pages/Book.js
+++ client/src/pages/Book.js
@@ -1,25 +1,49 @@
 import BookImage from '../components/BookImage';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 export default function Book() {
 
+    const [inputData, setInputData] = useState({
+        name: "",
+        author: "",
+        condition: "",
+        price: "",
+        edition: "",
+        semester: "",
+        subject: ""
+    })
+
     const isDarkMode = useSelector((state) => state.theme.isDarkMode);
 
+    const handleSubmit = async () => {
+        console.log("input data: ", inputData)
+    }
+
     return (
-        <div className="p-4" style={{height:'87vh'}}>
+        <div className="p-4" style={{ height: '87vh' }}>
             <div className="grid grid-cols-2 h-3/4 items-center">
                 <div className="flex flex-col items-center">
                     <BookImage />
                     <div className="mt-4">
-                        
+                        <input type="file" className="block w-full text-gray-800 text-sm
+
+                            file:mr-4 file:px-4 file:py-2 file:text-sm file:border-0
+
+                            file:rounded-full file:font-semibold file:text-violet-700 file:bg-violet-50
+
+                            hover:file:bg-amber-100 hover:file:cursor-pointer" />
                     </div>
                 </div>
                 <div className='flex flex-col gap-4 justify-center'>
-                    <input style={isDarkMode ? {backgroundColor:'#303134', color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type="text" placeholder="Name" className='border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none'/>
-                    <input style={isDarkMode ? {backgroundColor:'#303134', color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type="text" placeholder="Author" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
-                    <input style={isDarkMode ? {backgroundColor:'#303134', color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type="select" placeholder="Condition" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
-                    <input style={isDarkMode ? {backgroundColor:'#303134', color:'white'} : {backgroundColor:'#e9e9e9',color:'black'}} type="text" placeholder="Price" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none"/>
-                    <button className="bg-purple-600 text-white rounded-md p-2 w-3/4">Post Book</button>
+                    <input value={inputData.name} onChange={(e) => setInputData({ ...inputData, name: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="text" placeholder="Name" className='border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none' />
+                    <input value={inputData.author} onChange={(e) => setInputData({ ...inputData, author: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="text" placeholder="Author" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
+                    <input value={inputData.condition} onChange={(e) => setInputData({ ...inputData, condition: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="select" placeholder="Condition" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
+                    <input value={inputData.price} onChange={(e) => setInputData({ ...inputData, price: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="text" placeholder="Price" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
+                    <input value={inputData.edition} onChange={(e) => setInputData({ ...inputData, edition: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="text" placeholder="Edition" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
+                    <input value={inputData.semester} onChange={(e) => setInputData({ ...inputData, semester: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="text" placeholder="Semester" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
+                    <input value={inputData.subject} onChange={(e) => setInputData({ ...inputData, subject: e.target.value })} style={isDarkMode ? { backgroundColor: '#303134', color: 'white' } : { backgroundColor: '#e9e9e9', color: 'black' }} type="text" placeholder="Subject" className="border border-gray-400 rounded-md p-2 w-3/4 pl-4 focus:outline-none" />
+                    <button className="bg-purple-600 text-white rounded-md p-2 w-3/4" onClick={handleSubmit}>Post Book</button>
                 </div>
             </div>
         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,10 +8,19 @@ function Home() {
   return (
     <div className='p-4'>
 
-      <div className='mt-4 flex gap-4 items-center '>
-        <SearchBar />
-        <span><button className='border border-purple-500 lg:mr-5 rounded-full px-6 hover:bg-purple-500 py-2 flex items-center gap-2'>filter<IoFilter /></button></span>
+      <div className='flex justify-between'>
+
+        <div className='mt-4 flex gap-4 items-center '>
+          <SearchBar />
+          <span><button className='border border-green-500 lg:mr-5 rounded-full px-6 hover:bg-purple-500 py-2 flex items-center gap-2'>filter<IoFilter /></button></span>
+        </div>
+
+        <div className='flex items-end'>
+          <Link to="/book" className='bg-green-600 text-white rounded-md p-2 px-8'>Sell</Link>
+        </div>
+
       </div>
+
 
       <div className='mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10'>
 
