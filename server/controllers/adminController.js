3ca8faf6c6fe8be25c00e4977a9844338522faa2
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-01-17 22:17:46 +0500

    Added delete user function in admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -7,7 +7,7 @@ const getAllUsers = async (req, res) => {
 
         if(userRole === 'admin') {
             const allUsers = await pool.query('SELECT * FROM users');
-            res.status(200).json({message: "Welcome to admin dashboard", users: allUsers.rows});
+            res.status(200).json(allUsers.rows);
         }
         else {
             res.status(400).json({error:"cannot access this route"})
@@ -26,7 +26,7 @@ const getAllBooks = async (req,res) => {
 
         if(userRole === 'admin') {
             const allBooks = await pool.query('SELECT * FROM book_details');
-            res.status(200).json({message: "Welcome to admin dashboard", books: allBooks.rows});
+            res.status(200).json(allBooks.rows);
         }
         else {
             res.status(400).json({error:"cannot access this route"})
@@ -38,4 +38,42 @@ const getAllBooks = async (req,res) => {
     }
 }
 
-module.exports = { getAllUsers, getAllBooks };
+const deleteBook = async (req, res) => {
+    try {
+        console.log("in delete boko")
+        const userRole = req.user.role;
+        if(userRole === 'admin') {
+            const { bID } = req.query;
+            const query = `DELETE FROM book_details WHERE bID = $1 RETURNING *`;
+            const deletedBook = await pool.query(query, [bID]);
+            return res.json({ message: "Book deleted successfully" });
+        }
+        else {
+            res.status(400).json({error:"cannot access this route"})
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+const deleteUser = async(req,res) => {
+    try {
+        const userRole = req.user.role;
+        if(userRole === 'admin') {
+            const {uID} = req.query
+            const query = `DELETE FROM users WHERE id = $1 RETURNING *`;
+            const deletedUser = await pool.query(query, [uID]);
+            return res.json({ message: "User deleted successfully" });
+        }
+        else {
+            res.status(400).json({error:"cannot access this route"})
+        }
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500).json({error:"Internal Server Error"})
+    }
+}
+
+module.exports = { getAllUsers, getAllBooks, deleteBook, deleteUser };
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
-const {getAllUsers, getAllBooks} = require("../controllers/adminController");
+const {getAllUsers, getAllBooks, deleteUser} = require("../controllers/adminController");
+const { deleteBook } = require("../controllers/adminController");
 const verifyRole = require("../middleware/verifyRole");
 
 //protected routes
 router.get("/users",verifyRole('admin'),getAllUsers);
 router.get("/books",verifyRole("admin"),getAllBooks);
+router.delete("/books",verifyRole('admin'),deleteBook);
+router.delete("/users",verifyRole('admin'),deleteUser);
 
 module.exports = router;
