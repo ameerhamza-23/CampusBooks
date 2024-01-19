a3f49438e164752715e9d2e7150185fa9dc44067
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-01-19 19:42:16 +0500

    Added getBook function in book controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -2,88 +2,101 @@ const pool = require("../db");
 
 // create book
 const createBook = async (req, res) => {
-    try {
-        const { name, author, price, condition, uID, edition, semester, subject } = req.body;
-        const query = `INSERT INTO book_details (name, author, price, condition, uID, edition, semester, subject) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
-        const values = [name, author, price, condition, uID, edition, semester, subject];
-        const newBook = await pool.query(query, values);
-        return res.json({ message: "Book created successfully" });
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const { name, author, price, condition, uID, edition, semester, subject } = req.body;
+    const query = `INSERT INTO book_details (name, author, price, condition, uID, edition, semester, subject) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+    const values = [name, author, price, condition, uID, edition, semester, subject];
+    const newBook = await pool.query(query, values);
+    return res.json({ message: "Book created successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // delete book
 const deleteBook = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const query = `DELETE FROM books WHERE bID = $1 RETURNING *`;
-        const deletedBook = await pool.query(query, [id]);
-        return res.json({ message: "Book deleted successfully" });
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const { id } = req.params;
+    const query = `DELETE FROM books WHERE bID = $1 RETURNING *`;
+    const deletedBook = await pool.query(query, [id]);
+    return res.json({ message: "Book deleted successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // add to wishlist
 const addToWishlist = async (req, res) => {
-    try {
-        const { bID, uID } = req.query;
-        const query = `INSERT INTO wishlist (bID, uID) VALUES ($1, $2) RETURNING *`;
-        const values = [bID, uID];
-        const result = await pool.query(query, values);
-        return res.json({ message: "Added to wishlist successfully" });
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const { bID, uID } = req.query;
+    const query = `INSERT INTO wishlist (bID, uID) VALUES ($1, $2) RETURNING *`;
+    const values = [bID, uID];
+    const result = await pool.query(query, values);
+    return res.json({ message: "Added to wishlist successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // remove from wishlist
 const removeFromWishlist = async (req, res) => {
-    try {
-        const { bID, uID } = req.query;
-        const query = `DELETE FROM wishlist WHERE bID = $1 AND uID = $2 RETURNING *`;
-        const values = [bID, uID];
-        const result = await pool.query(query, values);
-        return res.json({ message: "Removed from wishlist successfully" });
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const { bID, uID } = req.query;
+    const query = `DELETE FROM wishlist WHERE bID = $1 AND uID = $2 RETURNING *`;
+    const values = [bID, uID];
+    const result = await pool.query(query, values);
+    return res.json({ message: "Removed from wishlist successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 const getWishlist = async (req, res) => {
-    try {
-        const { uID } = req.query; // Use req.query to get parameters from the URL
-        const query = `
+  try {
+    const { uID } = req.query; // Use req.query to get parameters from the URL
+    const query = `
             SELECT w.*, bd.name AS book_name, bd.author, bd.price, bd.condition, bd.edition, bd.semester, bd.subject
             FROM wishlist w
             JOIN book_details bd ON w.bID = bd.bid
             WHERE w.uID = $1
         `;
-        const values = [uID];
-        const wishlistWithDetails = await pool.query(query, values);
-        return res.json(wishlistWithDetails.rows);
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    const values = [uID];
+    const wishlistWithDetails = await pool.query(query, values);
+    return res.json(wishlistWithDetails.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 const getAllBooks = async (req, res) => {
-    try {
+  try {
 
-        const allBooks = await pool.query('SELECT * FROM book_details');
-        return res.status(200).json(allBooks.rows);
+    const allBooks = await pool.query('SELECT * FROM book_details');
+    return res.status(200).json(allBooks.rows);
 
-    }
-    catch (err) {
-        console.log(err.message)
-        return res.status(500).json({ error: "internal server error" })
-    }
+  }
+  catch (err) {
+    console.log(err.message)
+    return res.status(500).json({ error: "internal server error" })
+  }
 }
 
-module.exports = { createBook, deleteBook, addToWishlist, removeFromWishlist, getWishlist, getAllBooks };
+const getBook = async (req, res) => {
+
+  try {
+    const { id } = req.query
+    console.log("bookid: ", id)
+    return res.status(200).json({ message: "working" })
+  }
+  catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: "internal server error" })
+  }
+}
+
+module.exports = { createBook, deleteBook, addToWishlist, removeFromWishlist, getWishlist, getAllBooks, getBook };
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
-const { createBook, deleteBook, addToWishlist, removeFromWishlist, getWishlist, getAllBooks } = require("../controllers/bookController");
+const { createBook, deleteBook, addToWishlist, removeFromWishlist, getWishlist, getAllBooks, getBook } = require("../controllers/bookController");
 
-router.get("/",getAllBooks);
+router.get("/", getAllBooks);
 router.post("/create", createBook);
-router.delete("/delete/:id", deleteBook); 
+router.delete("/delete/:id", deleteBook);
+router.get("/:id", getBook);
 
-router.post("/save", addToWishlist); 
-router.delete("/remove", removeFromWishlist); 
+router.post("/save", addToWishlist);
+router.delete("/remove", removeFromWishlist);
 router.get("/wishlist", getWishlist);
 
 module.exports = router;
